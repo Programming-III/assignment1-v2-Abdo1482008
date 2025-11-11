e4dc22a4e453a2a0b42fcf365c964e78b7735998
Abdo1482008 <abdelrahman.aanwar@gmail.com>
2025-11-11 09:29:25 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,7 +8,38 @@
 
 using namespace std;
 
-// ==================== Person Class Implementation =========================
+#include <iostream>
+
+using namespace std;
+class Person {
+    public:
+    string name;
+    int id;
+
+void displau(){
+    cout << name << endl;
+    cout << id << endl;
+
+}
+
+
+
+
+
+}
+class Student :: Person {
+    public:
+    int yearLevel;
+    string major;
+
+    Student(int y, string e){
+        yearLevel = y;
+       major = e;
+    }
+     void display(){
+        cout << yearLevel << endl;
+        cout << major << endl;
+    }
 
 
 
@@ -16,32 +47,50 @@ using namespace std;
 
 
 
+}
 
-// ==================== Student Class Implementation ====================
+class Course :: Student {
+    private: 
+    string CourseCode;
+    string CourseName;
+    int maxstudents;
+    Student* Student;
+    int currentstatus;
+    
+    void addStudent(const Student& s){
+        Student.add(s);
+    }
+    void displayCourseInfo();
+    cout << Student.display()<<endl;
+    cout << CourseCode << endl;
+    cout << CourseName << endl;
+    cout << maxstudents << endl;
+    cout << currentstatus << endl;
+
+}
+class instructor:: Person {
+    public:
+    string department;
+    int experienceYears;
+
+    void display(){
+ cout << department << endl;
+        cout << experienceYears << endl;
+    }
+    
 
 
 
-
-
-
-// ==================== Instructor Class Implementation ====================
-
-
-
-
-
-
-// ==================== Course Class Implementation ====================
-
-
-
-
-
-
-
-
-// ==================== Main Function ====================
+}
 int main() {
+   Student s  = new Student(5 , "cs");
+   addStudent(s);
+   instructor i = new instructor("sales" , 5);
+   Course c  new Course("CSIN" , "PROG" , 50 , 2);
+   c.displayCorseInfo();
+   s.display();
+   i.display();
+
    
     
     return 0;
